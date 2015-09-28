b48748b9ab74cd640d905d5f81f528f32315f70b
Robert Mealey <robertemealey@gmail.com>
2015-09-28 04:20:18 +0000

    basics

---
+++ test_cases.md
@@ -0,0 +1,9 @@
+a brief, sordid love affair with a cat owner, that ends without rancor
+when a word conveys a subjective sense opposite or unrelated to its meaning, to a man 
+when a word conveys a subjective sense opposite or unrelated to its meaning, to a woman 
+the feeling one gets when a brief window of opportunity passes without being taken advantage of
+an insulting, slightly vulgar term for male convention attendees 
+an excellent smelling, poorly lit space
+to strive forever towards a goal, without ever fully attaining it or completely failing to
+doing something mundane or boring with surprising panache and creativity
+the relationship between two people who pass each other on the street every day but never speak 
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import random
 import spacy
 import numpy as np
 import time
+import sys
 from spacy.en import English
 from string import lowercase
 from collections import defaultdict, Counter
@@ -35,17 +36,21 @@ def iter_sample_fast(iterable, samplesize):
     return results
 
 
-stops = [
-        'of', 'or', 'for', 'a', 'the', 'as', 'but', 'to',
-        'am', 'an', 'are', 'in', 'with', 'and', 'that', 'is',
-        ]
-def preprocess_synset(ss, char_ref, pos_ref):
+#stops = [
+#        'of', 'or', 'for', 'a', 'the', 'as', 'but', 'to',
+#        'am', 'an', 'are', 'in', 'with', 'and', 'that', 'is',
+#        ]
+stops = []
+def preprocess_synset(ss, char_ref, pos_ref, char_def_ref):
+    if any([c not in lowercase for c in ss.name().split('.')[0]]):
+        return "Invalid"
     d = [
             [char_ref[l] for l in list(ss.name().split('.')[0])],
             pos_ref[ss.pos()], 
             [t.orth_.lower() for t in nlp(ss.definition())]
         ]
     d = d + [[w for w in d[2] if w not in stops],]
+    d = d + [[char_def_ref[l] for l in list(ss.definition())],]
     return d
 
 def preprocess_defin(d, word_ref):
@@ -75,8 +80,8 @@ def rm_invalid_and_zero_pad(seq, max_len, lpad=True):
 char_ref = defaultdict(lambda: char_ref.__len__() + 1,
         {l:i+1 for i, l in enumerate(list(unicode(lowercase)))})
 pos_ref = defaultdict(lambda: pos_ref.__len__())
+char_def_ref = defaultdict(lambda: char_def_ref.__len__() + 1)
 
-import sys
 sample = int(sys.argv[1])
 
 if sample > 0:
@@ -84,7 +89,8 @@ if sample > 0:
 else:
     defs = wn.all_synsets()
 
-defs = [preprocess_synset(ss, char_ref, pos_ref) for ss in defs]
+defs = [preprocess_synset(ss, char_ref, pos_ref, char_def_ref) for ss in defs]
+defs = [d for d in defs if d != 'Invalid']
 print(len(defs))
 word_count = Counter(chain(*[d[3] for d in defs]))
 word_ref = {w1:i+1 for i, w1 in enumerate((w2 for w2, c in word_count.items()
@@ -95,63 +101,43 @@ defs = [preprocess_defin(d, word_ref) for d in defs]
 ## generate feature index to feature mappings
 rev_word_ref = {v:k for k,v in word_ref.items()}
 rev_char_ref = {v:k for k,v in char_ref.items()}
+rev_char_def_ref = {v:k for k,v in char_def_ref.items()}
 rev_pos_ref = {v:k for k,v in pos_ref.items()}
 char_ref.default_factory = None
+char_def_ref.default_factory = None
 pos_ref.default_factory = None
 word_ref['__PADDING__'] = 0
 char_ref['__PADDING__'] = 0
+char_def_ref['__PADDING__'] = 0
 
 print(''.join([rev_char_ref[x] for x in defs[0][0] if x != 0]))
 print(' '.join([rev_word_ref[x] for x in defs[0][3] if x != 0]))
+print(''.join([rev_char_def_ref[x] for x in defs[0][4] if x != 0]))
 
 max_word_len = max([len(x[0]) for x in defs])
 max_def_len = max([len(x[3]) for x in defs])
+max_def_len_char = max([len(x[4]) for x in defs])
 for d in defs:
     d[0] = rm_invalid_and_zero_pad(d[0], max_word_len, lpad=False)
     d[3] = rm_invalid_and_zero_pad(d[3], max_def_len)
+    d[4] = rm_invalid_and_zero_pad(d[4], max_def_len_char)
 
 print(''.join([rev_char_ref[x] for x in defs[0][0] if x != 0]))                                                                                                       
 print(' '.join([rev_word_ref[x] for x in defs[0][3] if x != 0])) 
+print(''.join([rev_char_def_ref[x] for x in defs[0][4] if x != 0]))
 
 random.shuffle(defs)
 n_defs = len(defs)
-train = defs[0:int(0.8*n_defs)]
-val = defs[int(0.8*n_defs):int(0.9*n_defs)]
+train = defs[0:int(0.9*n_defs)]
 test = defs[int(0.9*n_defs):]
 X_train = np.vstack([x[3] for x in train])
-X_val = np.vstack([x[3] for x in val])
 X_test = np.vstack([x[3] for x in test])
+X_train_char = np.vstack([x[4] for x in train])
+X_test_char = np.vstack([x[4] for x in test])
 
 if False:
-    ## model 1: classify nouns based on definition
-    y1_train = np.array([rev_pos_ref[x[1]]=='n' for x in train])
-    y1_val = np.array([rev_pos_ref[x[1]]=='n' for x in val])
-    y1_test = np.array([rev_pos_ref[x[1]]=='n' for x in test])
-    checkpointer1 = ModelCheckpoint(filepath="/tmp/weights1.hdf5", verbose=1, save_best_only=True)
-    early_stop1 = EarlyStopping(monitor='val_loss', patience=2, verbose=0)
-    model1 = Sequential()
-    model1.add(Embedding(len(word_ref), 256, mask_zero=True))
-    model2.add(LSTM(256, 256, activation='sigmoid', inner_activation='hard_sigmoid'))
-    model1.add(Dropout(0.5))
-    model1.add(Dense(256, 1))
-    model1.add(Activation('sigmoid'))
-    model1.compile(loss='binary_crossentropy', optimizer='rmsprop')
-    model1.fit(X_train, y1_train, batch_size=1024, nb_epoch=15, verbose=1,
-            validation_data=(X_val, y1_val), callbacks=[checkpointer1, early_stop1])
-    ## final score
-    score = model1.evaluate(X_test, y1_test, batch_size=1024)
-    print(score)
-    preds = model1.predict(X_test, batch_size=1024)
-    print(classification_report(y1_test, preds>0.5))
-    ## best val score
-    model1.load_weights('/tmp/weights1.hdf5')
-    score = model1.evaluate(X_test, y1_test, batch_size=1024)
-    print(score)
-    preds = model1.predict(X_test, batch_size=1024)
-    print(classification_report(y1_test, preds>0.5))
-
-
-if False:
+    val = test[0:(len(test)/2)]
+    test = test[(len(test)/2):]
     HIDDEN = 512
     ## model 2: multi-class part-of-speech classification based on definition
     y2_train = np.array([label_to_multiclass(x[1], len(pos_ref)) for x in train])
@@ -198,73 +184,84 @@ if False:
 
 
 ## model 3: predict character vector
-y3_train = np.array([np.array([label_to_multiclass(y, len(char_ref)) for y in x[0]]) for x in train])
-y3_val = np.array([np.array([label_to_multiclass(y, len(char_ref)) for y in x[0]]) for x in val])
-y3_test = np.array([np.array([label_to_multiclass(y, len(char_ref)) for y in x[0]]) for x in test])
+y3_train = np.array([np.array([label_to_multiclass(y, len(char_ref))
+    for y in x[0]]) for x in train])
+y3_test = np.array([np.array([label_to_multiclass(y, len(char_ref))
+    for y in x[0]]) for x in test])
 
 #The Magictionary
 #The Imaginictionary 
 #The Deeptionary
 #The Dranktionary
 
-BATCH_SIZE = 128
-HIDDEN_LAYER = 512
-model3 = Sequential()
-model3.add(Embedding(len(word_ref), HIDDEN_LAYER, mask_zero=True))
-model3.add(LSTM(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', inner_activation='hard_sigmoid'))
-model3.add(Dropout(0.2))
-model3.add(RepeatVector(max_word_len))
-model3.add(LSTM(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', inner_activation='hard_sigmoid', return_sequences=True))
-model3.add(Dropout(0.2))
-model3.add(LSTM(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', inner_activation='hard_sigmoid', return_sequences=True))
-model3.add(Dropout(0.2))
-model3.add(TimeDistributedDense(HIDDEN_LAYER, len(char_ref)))
-model3.add(Activation('softmax'))
-model3.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+if True:
+    HIDDEN_LAYER = 512
+    model3 = Sequential()
+    model3.add(Embedding(len(word_ref), HIDDEN_LAYER, mask_zero=True))
+    model3.add(JZS3(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh'))
+    model3.add(Dropout(0.1))
+    model3.add(RepeatVector(max_word_len))
+    model3.add(JZS3(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', return_sequences=True))
+    model3.add(Dropout(0.1))
+    model3.add(JZS3(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', return_sequences=True))
+    model3.add(Dropout(0.1))
+    model3.add(JZS3(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', return_sequences=True))
+    model3.add(Dropout(0.1))
+    model3.add(TimeDistributedDense(HIDDEN_LAYER, len(char_ref)))
+    model3.add(Activation('softmax'))
+    model3.compile(loss='categorical_crossentropy', optimizer='adam')
 
-preds = []
-checkpointer3 = ModelCheckpoint(filepath="/tmp/weights3.hdf5", verbose=1, save_best_only=True)
-early_stop3 = EarlyStopping(monitor='val_loss', patience=2, verbose=0)
+    preds = []
+    checkpointer3 = ModelCheckpoint(filepath="/tmp/weights3.hdf5", verbose=1, save_best_only=True)
+    early_stop3 = EarlyStopping(monitor='val_loss', patience=2, verbose=0)
 
-model3.fit(X_train, y3_train, batch_size=32, nb_epoch=2, verbose=1,
-    validation_data=(X_val, y3_val), callbacks=[checkpointer3, early_stop3])
-score = model3.evaluate(X_test, y3_test, batch_size=BATCH_SIZE)
-print(score)
-preds = preds + [model3.predict(X_test, batch_size=BATCH_SIZE),]
-for j in [0, X_test.shape[1]/2, X_test.shape[1]]:
-    for x in range(len(preds)):
-        print(''.join([rev_char_ref[i] for i in preds[x][j].argmax(1) if i != 0]), ' '.join([rev_word_ref[i] for i in X_test[j] if i != 0]))
-
-
-model3.fit(X_train, y3_train, batch_size=64, nb_epoch=2, verbose=1,
-    validation_data=(X_val, y3_val), callbacks=[checkpointer3, early_stop3])
-score = model3.evaluate(X_test, y3_test, batch_size=BATCH_SIZE)
-print(score)
-preds = preds + [model3.predict(X_test, batch_size=BATCH_SIZE),]
-for j in [0, X_test.shape[1]/2, X_test.shape[1]]:
-    for x in range(len(preds)):
-        print(''.join([rev_char_ref[i] for i in preds[x][j].argmax(1) if i != 0]), ' '.join([rev_word_ref[i] for i in X_test[j] if i != 0]))
-
-while True:
-    model3.fit(X_train, y3_train, batch_size=BATCH_SIZE, nb_epoch=5, verbose=1,
-        validation_data=(X_val, y3_val), callbacks=[checkpointer3, early_stop3])
-    score = model3.evaluate(X_test, y3_test, batch_size=BATCH_SIZE)
-    print(score)
-    preds = preds + [model3.predict(X_test, batch_size=BATCH_SIZE),]
-    for j in [0, X_test.shape[1]/2, X_test.shape[1]]:
-        for x in range(len(preds)):
-            print(''.join([rev_char_ref[i] for i in preds[x][j].argmax(1) if i != 0]), ' '.join([rev_word_ref[i] for i in X_test[j] if i != 0]))
-
+    iteration = 0 
+    BATCH_SIZE = 32 
+    for _ in range(40):
+        iteration = iteration + 1
+        print('-- iteration {} --'.format(iteration))
+        model3.fit(X_train, y3_train, batch_size=BATCH_SIZE, nb_epoch=1, verbose=1)
+        model3.save_weights('model3p3_iteration_{}.hdf5'.format(iteration), overwrite=True)
+        preds = preds + [model3.predict(X_test, batch_size=BATCH_SIZE),]
+        for j in [0, X_test.shape[0]/2, X_test.shape[0]-1]:
+            for x in range(max(0, len(preds)-5), len(preds)):
+                print(
+                        ''.join([rev_char_ref[i] for i in preds[x][j].argmax(1) if i != 0]), 
+                        ' '.join([rev_word_ref[i] for i in X_test[j] if i != 0])
+                        )
 
 
 if False:
-    ## last model
-    score = model3.evaluate(X_test, y3_test, batch_size=32)
-    print(score)
-    preds_last = model3.predict(X_test, batch_size=32)
+    HIDDEN_LAYER = 512
+    model4 = Sequential()
+    model4.add(TimeDistributedDense(len(char_ref), HIDDEN_LAYER))
+    model4.add(JZS2(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh'))
+    model4.add(Dropout(0.2))
+    model4.add(RepeatVector(max_word_len))
+    model4.add(JZS2(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', return_sequences=True))
+    model4.add(Dropout(0.2))
+    model4.add(JZS2(HIDDEN_LAYER, HIDDEN_LAYER, activation='tanh', return_sequences=True))
+    model4.add(Dropout(0.2))
+    model4.add(TimeDistributedDense(HIDDEN_LAYER, len(char_ref)))
+    model4.add(Activation('softmax'))
+    model4.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    ## best model
-    model3.load_weights('/tmp/weights3.hdf5')
-    score = model3.evaluate(X_test, y3_test, batch_size=32)
-    preds = model3.predict(X_test, batch_size=32)
+    preds = []
+    checkpointer4 = ModelCheckpoint(filepath="/tmp/weights4.hdf5", verbose=1, save_best_only=True)
+    early_stop4 = EarlyStopping(monitor='val_loss', patience=2, verbose=0)
+
+    iteration = 0
+    BATCH_SIZE = 64 
+    for _ in range(40):
+        iteration = iteration + 1
+        print('-- iteration {} --'.format(iteration))
+        model4.fit(X_train_char, y3_train, batch_size=BATCH_SIZE, nb_epoch=1, verbose=1)
+        model4.save_weights('model4_iteration_{}.hdf5'.format(iteration))
+        preds = preds + [model4.predict(X_test_char, batch_size=BATCH_SIZE),]
+        for j in [0, X_test_char.shape[0]/2, X_test_char.shape[0]-1]:
+            for x in range(max(0, len(preds)-5), len(preds)):
+                print(
+                    ''.join([rev_char_ref[i] for i in preds[x][j].argmax(1) if i != 0]),
+                    ''.join([rev_char_def_ref[i] for i in X_test_char[j] if i != 0])
+                    )
 
